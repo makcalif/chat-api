6c32d26b0df7990d5d0aab131c102d7762c3cd1a
Mumtaz <makcalif@yahoo.com>
2025-06-15 20:33:26 -0600

    Initial commit: Terraform configuration for chat-api

---
+++ api_gateway.tf
@@ -0,0 +1,45 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "chat-api"
+  description = "FastAPI deployed to Lambda via API Gateway"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_integration.lambda))
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.lambda
+  ]
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
---
+++ lambda.tf
@@ -0,0 +1,31 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "chat-api-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "api" {
+  function_name = "chat-api-lambda"
+  runtime       = "python3.10"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "main.lambda_handler"  # Change if your entry point differs
+  filename      = "${path.module}/deployment-package.zip"  # Make sure to zip your app
+
+  source_code_hash = filebase64sha256("${path.module}/deployment-package.zip")
+
+  timeout = 30
+}
